9c676b98f870abda8c6db1846bb65a4ebfc7e8a1
Roppin.L <robin.ljungqvist@gmail.com>
2016-05-30 09:59:53 +0200

    Added images for the project

--- Bootstrap_Island/scripts/defaultJS.js
+++ Bootstrap_Island/scripts/defaultJS.js
@@ -1,14 +1,1 @@
-﻿var affixElement = '#navigationbar';
-
-$(affixElement).affix({
-    offset: {
-        // Distance of between element and top page
-        top: function () {
-            return (this.top = $(affixElement).offset().top)
-        },
-        // when start #footer 
-        bottom: function () {
-            return (this.bottom = $('#footer').outerHeight(true))
-        }
-    }
-});
+﻿
